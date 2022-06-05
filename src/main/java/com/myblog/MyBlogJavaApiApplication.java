d7555082c2f41df4fd67742ee8b08c3b2f6804c2
Micheal Limones <micheal@zestylimones.com>
2022-06-05 09:20:06 -0500

    inital commit

---
+++ src/main/java/com/myblog/MyBlogJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.myblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBlogJavaApiApplication.class, args);
+	}
+
+}
