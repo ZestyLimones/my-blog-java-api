861bf74db61e9fb2e6730470c0f1f2261e63a38e
Micheal Limones <micheal@zestylimones.com>
2022-06-13 10:45:22 -0500

    removed HelloWOrld.

--- src/main/java/com/myblog/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.myblog;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
--- src/main/java/com/myblog/VariablesPractice.java
+++ src/main/java/com/myblog/VariablesPractice.java
@@ -1,5 +1,11 @@
 package com.myblog;
 
 public class VariablesPractice {
-    
+    public static void main(String[] args) {
+        int num = 12;
+        double num2 = 5.67;
+        double sum = num + num2;
+        String name = "Steve";
+
+    }
 }
